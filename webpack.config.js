5e1960ec4b86e39db2c99468e42a54d8cf74d0b5
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2021-05-18 18:42:12 +0300

    add eslint, update dependencies webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,37 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-
-const pug = {
-    test: /\.pug$/,
-    use: ['pug-loader']
-  }
+const path = require("path");
 
 const js = {
     test: /\.js$/,
     exclude: /node_modules/,
-    use: ['babel-loader']
-}
-
-const images = {
-  test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-  type: 'asset/images',
-}
-
-const css = {
-  test: /\.(scss|css)$/,
-  use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-}
-
-const fonts = {
-  test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-  type: 'asset/inline',
-}
+    use: {
+        loader: "babel-loader",
+        query: {
+            presets: [
+                ["@babel/preset-env", { modules: false }]
+            ]
+        }
+    }
+};
 
 module.exports = {
-  entry: {
-    main: path.resolve(__dirname, './src/index.js'),
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].bundle.js',
-  },
-  mode: 'development',
-  devServer: {
-    historyApiFallback: true,
-    contentBase: path.resolve(__dirname, './dist'),
-    open: true,
-    compress: true,
-    hot: true,
-    port: 8080,
-  },
+    entry: {
+        main: path.resolve(__dirname, "./src/js/index.js"),
+    },
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "[name].js",
+        chunkFilename: "[name].js",
+        publicPath: "/"
+    },
 
-  module: {
-    rules: [pug, js, images, css, fonts]
-  },
+    module: {
+        rules: [js]
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/index.pug'), 
-      filename: 'index.html', 
-      inject: true
-    }),
-    new CleanWebpackPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
-}
+    resolve: {
+        alias: {
+            "%modules%": path.resolve(__dirname, "src/blocks/modules"),
+            "%components%": path.resolve(__dirname, "src/blocks/components")
+        }
+    },
+};
