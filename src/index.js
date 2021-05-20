89648d5c4bb103faf983d5c9718073c4e3b0b01b
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2021-05-20 22:49:52 +0300

    add files for published

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './styles/main.scss'
-
-// Create heading node
-const heading = document.createElement('h1')
-heading.textContent = 'Interesting!'
-
-// Append heading node to the DOM
-const app = document.querySelector('#root')
-app.append(heading)
